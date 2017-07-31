37e9bbeecf0119a9ddad725e54869b510c297bd2
Ronald Jack Jenkins Jr <me@ronjenkins.info>
2017-07-30 21:49:01 -0400

    Fix deploy missing creds

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+mvn clean
+
+set -o allexport
+source .env
+set +o allexport
+rm .env
+
+mvn -s settings.xml -Drtr.release=true -P release
+
+git config --global user.email "me@ronjenkins.info"
+git config --global user.name "Ronald Jack Jenkins Jr."
+
+POM_VERSION=$(cat .version | xargs)
+
+git add pom.xml
+git commit -m "Committing release $POM_VERSION"
+
+git tag "$POM_VERSION"
+git push origin "$POM_VERSION"
